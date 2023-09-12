6269cc89ca7e1689d4040c9bdf7a1a985bf3d22b
matplo <ploskon@gmail.com>
2023-09-12 11:21:03 -0700

    move definition of the jet graph elsewhere

--- heppyy/sandbox/visual/jet_draw/jet_graph_fig.py
+++ heppyy/sandbox/visual/jet_draw/jet_graph_fig.py
@@ -2,46 +2,6 @@ import networkx as nx
 import plotly.graph_objects as go
 
 
-class JetGraph(object):
-	def __init__(self, j):
-		self.j = j
-		self.G = nx.Graph()
-		self.n = 0
-		self.max_depth = 0
-		self.max_depth = self.get_max_depth(j, 0)
-		self.add_sj(j, -1, self.max_depth)
-  
-	def get_max_depth(self, sj, depth):
-		p1 = fj.PseudoJet()
-		p2 = fj.PseudoJet()
-		depth = depth + 1
-		if sj.has_parents(p1, p2):
-			_ = self.get_max_depth(p1, depth)
-			_ = self.get_max_depth(p2, depth)
-		if depth > self.max_depth:	
-			self.max_depth = depth
-		return self.max_depth
-   
-	def add_sj(self, sj, parent_n, depth):
-		p1 = fj.PseudoJet()
-		p2 = fj.PseudoJet()
-		self.n = self.n + 1
-		this_n = self.n
-		dphi = self.j.delta_phi_to(sj)
-		deta = self.j.eta() - sj.eta()
-		if sj.has_parents(p1, p2):
-			# self.G.add_node(this_n, color='red', p=(sj.px(), sj.py(), sj.pz()))
-			# self.G.add_node(this_n, color='red', p=(sj.phi(), sj.eta(), sj.perp()))
-			self.G.add_node(this_n, color='red', p=(dphi, deta, sj.perp()), symbol='square-open', size=15)
-			if parent_n > 0:
-				self.G.add_edge(this_n, parent_n, color='green')
-			self.add_sj(p1, this_n, depth)
-			self.add_sj(p2, this_n, depth)
-		else:
-			self.G.add_node(this_n, color='blue', p=(dphi, deta, sj.perp()), symbol='circle', size=15)
-			if parent_n > 0:
-				self.G.add_edge(this_n, parent_n, color='green')
-
 def draw_jet_graph(G, title='a jet'):
     node_x = [attrs.get('p', (0,0,0))[0] for node, attrs in G.nodes(data=True)]
     node_y = [attrs.get('p', (0,0,0))[1] for node, attrs in G.nodes(data=True)]
