bc3cc58103227cf669a5fe3c67f9a8fe3b36c4d0
matplo <ploskon@gmail.com>
2024-04-29 13:56:51 -0700

    enable/disable console output

--- heppyy/example/read_hybrid.py
+++ heppyy/example/read_hybrid.py
@@ -19,7 +19,8 @@ from cppyy.gbl.std import vector
 
 from cppyy.gbl import heppyy
 
-print(cppyy.gbl.__dict__)
+# print(cppyy.gbl.__dict__)
+from heppyy.util.logger import Logger
 
 def main():
 	parser = argparse.ArgumentParser(description='analyze hybrid with fastjet on the fly', prog=os.path.basename(__file__))
@@ -27,23 +28,36 @@ def main():
 	parser.add_argument('-n', '--nev', help='number of events', default=10, type=int)
 	parser.add_argument('-w', '--wake', help='include wake particles', action='store_true', default=False)
 	parser.add_argument('-v', '--verbose', help="be verbose", default=False, action='store_true')
+	parser.add_argument('--jet-min-pt', help="minimum pT jet to accept", default=5., type=float)	
+	parser.add_argument('--max-eta-jet', help="max eta of a jet to accept", default=2.5, type=float)	
+	parser.add_argument('--jet-R', help="jet R", default=0.4, type=float)	
+	parser.add_argument('-g', '--debug', help="write debug things", default=False, action='store_true')
 	args = parser.parse_args()
- 
+
+	# set up logging - this uses singleton Logger
+	log_level = 'DEBUG' if args.debug else 'WARNING'
+	log = Logger(console=args.verbose, level=log_level)
+	if args.verbose:
+		log.set_level('INFO')
+	if args.debug:
+		log.set_level('DEBUG')
+  
+	log.critical(args)
+  
 	fj.ClusterSequence.print_banner()
 	print()
 
 	# use the energy recombiner
 	ner = heppyy.NegativeEnergyRecombiner(1001)
-	print(ner.description())
+	log.critical(ner.description())
 
 	# set up our jet definition and a jet selector
-	jet_R0 = 0.4
+	jet_R0 = args.jet_R
 	jet_def = fj.JetDefinition(fj.antikt_algorithm, jet_R0)
 	if args.wake:
 		jet_def = fj.JetDefinition(fj.antikt_algorithm, jet_R0, ner)
-	jet_selector = fj.SelectorPtMin(100.0) * fj.SelectorAbsEtaMax(1)
-	# jet_selector = fj.SelectorPtMin(300.0) * fj.SelectorAbsEtaMax(1.)
-	print(jet_def)
+	jet_selector = fj.SelectorPtMin(args.jet_min_pt) * fj.SelectorAbsEtaMax(args.max_eta_jet)
+	log.critical(jet_def.description())
 
 	# jet_def_lund = fj.JetDefinition(fj.cambridge_algorithm, 1.0)
 	# lund_gen = fj.contrib.LundGenerator(jet_def_lund)
@@ -62,28 +76,37 @@ def main():
 		sparts = input.getParticlesStr()
 		spartons = input.getPartonsStr()
 		ev_info = input.info()
-		print(f'ev_info.weight: {ev_info.weight()}, ev_info.cross: {ev_info.cross()}, ev_info.x: {ev_info.x()}, ev_info.y: {ev_info.y()}')
-		if args.verbose:
-			print(f'event {i} has {len(partons)} partons')
-			for np in range(len(partons)):
-				print(f'- from psj: px={partons[np].px()}, py={partons[np].py()}, pz={partons[np].pz()}, E={partons[np].E()}, m={partons[np].m()}, ui={partons[np].user_index()}')
-				print(f'- from str: {spartons[np]}')
-			print(f'event {i} has {len(parts)} particles')
-			for np in range(len(parts)):
-				print(f'- from psj: px={parts[np].px()}, py={parts[np].py()}, pz={parts[np].pz()}, E={parts[np].E()}, m={parts[np].m()}, ui={parts[np].user_index()}')
-				print(f'- from str: {sparts[np]}')
+
 		jets = fj.sorted_by_pt(jet_selector(jet_def(parts)))
 		if len(jets) == 0:
 			continue
 
-		# match partons to jets
-		print(f'number of accepted jets: {len(jets)}')
+		log.info(f'* event {i} has {len(partons)} partons')
+		log.info(f'  ev_info.weight: {ev_info.weight()}, ev_info.cross: {ev_info.cross()}, ev_info.x: {ev_info.x()}, ev_info.y: {ev_info.y()}')
+		for np in range(len(partons)):
+			log.info(f'  parton pt: {partons[np].pt()} eta: {partons[np].eta()}')
+			log.debug(f'- from psj: px={partons[np].px()}, py={partons[np].py()}, pz={partons[np].pz()}, E={partons[np].E()}, m={partons[np].m()}, ui={partons[np].user_index()}')
+			log.debug(f'- from str: {spartons[np]}')
+		log.debug(f'event {i} has {len(parts)} particles')
+		for np in range(len(parts)):
+			log.debug(f'- from psj: px={parts[np].px()}, py={parts[np].py()}, pz={parts[np].pz()}, E={parts[np].E()}, m={parts[np].m()}, ui={parts[np].user_index()}')
+			log.debug(f'- from str: {sparts[np]}')
+
+		log.info(f'-> event {i} has {len(jets)} jets')
 		for j in jets:
-			print(f'jet pt: {j.pt()} eta: {j.eta()}')
+			log.info(f'   - jet pt: {j.pt()} eta: {j.eta()}')
+   
+		log.disable_console()
+		log.info('debugging... - this should not write to console but show up in the log file')
+		log.enable_console()
+
+		# match partons to jets
+		for j in jets:
+			log.debug(f'jet pt: {j.pt()} eta: {j.eta()}')
 			for np in range(len(partons)):
 				p = partons[np]
 				if j.delta_R(p) < 0.4:
-					print(f' - matched parton {np} with pt: {p.perp()} eta: {p.eta()}')
+					log.debug(f' - matched parton {np} with pt: {p.perp()} eta: {p.eta()}')
 
 if __name__ == '__main__':
 	main()
--- heppyy/util/logger.py
+++ heppyy/util/logger.py
@@ -16,36 +16,52 @@ class Logger:
         if self._initialized:
             return
         self._initialized = True
+        self.level = level
+        self.console = console
 
         # Include the PID in the log filename
-        log_file = f"{os.path.splitext(log_file)[0]}_{os.getpid()}{os.path.splitext(log_file)[1]}"
+        self.log_file = f"{os.path.splitext(log_file)[0]}_{os.getpid()}{os.path.splitext(log_file)[1]}"
 
         # Create a formatter
         # formatter = logging.Formatter('%(asctime)s name=%(name)-12s level=%(levelname)-8s module=%(module)s func=%(funcName)s: %(message)s')
-        formatter = logging.Formatter('%(asctime)s name=%(name)-12s level=%(levelname)-8s file=%(filename)s:%(lineno)d module=%(module)s func=%(funcName)s: %(message)s')
+        self.formatter = logging.Formatter('%(asctime)s name=%(name)-12s level=%(levelname)-8s file=%(filename)s:%(lineno)d module=%(module)s func=%(funcName)s: %(message)s')
 
         # Create a handler for the file
-        self.file_handler = logging.FileHandler(log_file, mode='w')
-        self.file_handler.setLevel(level)
-        self.file_handler.setFormatter(formatter)
+        self.file_handler = logging.FileHandler(self.log_file, mode='w')
+        self.file_handler.setLevel(self.level)
+        self.file_handler.setFormatter(self.formatter)
 
         # Create a logger
         self.logger = logging.getLogger()
-        self.logger.setLevel(level)
+        self.logger.setLevel(self.level)
         self.logger.addHandler(self.file_handler)
 
         # If console is True, create a handler for the console
-        if console:
-            self.console_handler = logging.StreamHandler()
-            self.console_handler.setLevel(level)
-            self.console_handler.setFormatter(formatter)
-            self.logger.addHandler(self.console_handler)
-                
-    def set_level(self, level):
-        self.logger.setLevel(level)
-        self.file_handler.setLevel(level)
+        if self.console:
+          self.enable_console()
+          
+    def enable_console(self):
         if hasattr(self, 'console_handler'):
-          self.console_handler.setLevel(level)
+            self.logger.critical('Console handler already exists')
+            return
+        self.console_handler = logging.StreamHandler()
+        self.console_handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.console_handler)
+        self.set_level(self.level)
+        self.console = True
+
+    def disable_console(self):
+        if hasattr(self, 'console_handler'):
+            self.logger.removeHandler(self.console_handler)
+            del self.console_handler
+            self.console = False
+                        
+    def set_level(self, level):
+        self.level = level
+        self.logger.setLevel(self.level)
+        self.file_handler.setLevel(self.level)
+        if hasattr(self, 'console_handler'):
+          self.console_handler.setLevel(self.level)
 
     def get_logger(self, module_name):
         return logging.getLogger(module_name)
